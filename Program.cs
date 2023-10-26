c1f441ab2403b00b7d10eba1f662ff29a6c56100
padrito0 <146373717+padrito0@users.noreply.github.com>
2023-10-26 12:06:51 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter an integer: ");
+        if (int.TryParse(Console.ReadLine(), out int num))
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"{num} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is odd.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+    }
+}
